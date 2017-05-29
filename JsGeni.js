be87cbc173483115903b3ba892cd566346f28d07
geni429 <geni429@gmail.com>
2017-05-29 11:58:42 +0900

    algorithm 테이블 추가

--- JsGeni.js
+++ JsGeni.js
@@ -1,8 +1,14 @@
 var arrayTable = document.getElementById("ArrayItemsTable");
+var algorithmTable = document.getElementById("AlgorithmItemsTable");
 arrayTable.style.display = "none";
+algorithmTable.style.display = "none";
 
 document.getElementById("ArrayItems").addEventListener("click", function(){
   arrayTable.style.display = "table";
+}, false);
+
+document.getElementById("AlgorithmItems").addEventListener("click", function(){
+  algorithmTable.style.display = "table";
 }, false);
 
 var notice_1 = function(){
