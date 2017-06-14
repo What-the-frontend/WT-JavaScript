917051ab66274417f21b3359e6dfb0368c840f63
geni429 <geni429@gmail.com>
2017-06-14 22:56:08 +0900

    React _ state내용 추가

--- React/react-example/src/components/App.js
+++ React/react-example/src/components/App.js
@@ -1,46 +1,12 @@
 //import = var React = require('react'); ES6
 import React from 'react';
-import Header from './Header';
-import List from './List';
 
 class App extends React.Component {
     render(){
-
         return (
-            <div>
-                <Header title={this.props.headerTitle}/>
-                <Content />
-            </div>
-        );
-    }
-}
+          <div>
 
-class Content extends React.Component{
-    render(){
-        var rows = [];
-        for(var i=0; i<5; i++){
-            rows.push(<Row key={i}/>);
-        }
-        return (
-            <table>
-                <tbody>
-                    {rows}
-                </tbody>
-            </table>
-        );
-    }
-}
-
-class Row extends React.Component{
-    render(){
-        return (
-             <tr>
-                <td>A</td>
-                <td>B</td>
-                <td>C</td>
-                <td>D</td>
-                <td>E</td>
-            </tr>
+          </div>
         );
     }
 }
