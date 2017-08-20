622ce8f4fbd246d4770f9218b7e5f3fa5897ff04
geni429 <geni429@gmail.com>
2017-08-20 11:12:07 +0900

    project 초기구성 완료

---
+++ WEBPAGE/jstudy/src/containers/Post.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+class Post extends Component{
+    
+}
+
+export default Post;
---
+++ WEBPAGE/jstudy/src/containers/index.js
@@ -0,0 +1,4 @@
+import Main from './Main';
+import Post from './Post';
+
+export {Main, Post};
