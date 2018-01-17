f5c38f90823bdae0428205059d60701ae25b10f5
geni429 <geni429@gmail.com>
2018-01-17 15:47:38 +0900

    Design module.js 내용 추가

--- JAVASCRIPT/Design/module.js
+++ JAVASCRIPT/Design/module.js
@@ -1,8 +1,19 @@
-(function() {
+var testModule = (function() {
   //private area
-  
+  var user = {
+    name: "name",
+    password: "password"
+  }
+
   return {
     //public area
-
+    getUserName: function() {
+      return user.name;
+    }
   }
 })();
+
+//"name"
+console.log(testModule.getUserName());
+//undefined
+console.log(testModule.user);
