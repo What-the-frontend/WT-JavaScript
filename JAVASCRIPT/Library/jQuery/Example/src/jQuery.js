bc50f636d1dbc8c1a01985255cb04779e63fe767
geni429 <geni429@gmail.com>
2018-07-12 23:08:30 +0900

    [Delete] jQuery, [Update] move Webpack directory /JAVASCRIPT/Library -> /Library

--- JAVASCRIPT/Library/jQuery/Example/src/jQuery.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("jQuery_1").addEventListener("click", function () {
-  $(document).ready(function () {
-    $("#jQuery_1").css('color', 'yellow');
-  });
-  console.log('jQuery start')
-}, false);
