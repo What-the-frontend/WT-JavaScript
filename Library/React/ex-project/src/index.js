15eec1a57775c2581a0433c031918f1cd0038ae3
geni429 <geni429@gmail.com>
2018-07-15 00:59:53 +0900

    [Delete] Library, divide repositories

--- Library/React/ex-project/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ComponentPerformance from './ComponentPerformance';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<ComponentPerformance />, document.getElementById('root'));
-registerServiceWorker();
